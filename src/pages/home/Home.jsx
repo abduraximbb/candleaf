f03bced617ffadb7c0dde17d8d727d8aa9042e0a
Abduraxim Berdiqobilov <abduraximberdiqobilov@Abduraxims-MacBook-Air.local>
2024-12-06 08:22:45 +0500

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import axios from "../../api";
+import Products from "../../components/products/Products";
+import Hero from "../../components/hero/Hero";
+
+const Home = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios
+      .get("/products")
+      .then((res) => setData(res?.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div id="home">
+      {/* <h2>Home</h2> */}
+      <Hero/>
+      <Products data={data?.products}/>
+    </div>
+  );
+};
+
+export default Home;
